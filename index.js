5374ef36c3eecfe392a65da8544a86058c7eb448
Ritta <brittanykelly33@gmail.com>
2022-05-02 09:36:59 -0400

    commit tree 4a741337a8d37dfcbf27b7427bd097293c14b0f7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById('main').remove()
+//main.remove()
+
+const newHeader = document.createElement("h1")
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "Ritta is the champion"
+
+
